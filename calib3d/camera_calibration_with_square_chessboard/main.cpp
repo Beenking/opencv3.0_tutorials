2705424ac7f11e4ad4333208409f07d87d8943b0
Beenking <beenkinger@gmail.com>
2014-12-31 22:01:42 +0800

    a half of calib3d...

---
+++ calib3d/camera_calibration_with_square_chessboard/main.cpp
@@ -0,0 +1,20 @@
+// run this .cpp after you have got camera.yml by excute calibration.exe which needs imagelist.xml
+#include <opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+	Mat img = imread(argv[1], cv::IMREAD_GRAYSCALE);
+
+	std::vector<cv::Point2f> imageCorners;
+	bool isfound = findChessboardCorners(img, Size(9,6), imageCorners);
+
+	FileStorage fs(argv[2], FileStorage::READ);
+	Mat intrinsics, distortion;
+	fs["camera_matrix"] >> intrinsics;
+	fs["distortion_coefficients"] >> distortion;
+
+	std::vector<Point3f> boardPoints;
+	solvePnP(Mat(boardPoints), Mat(foundBoardCorners), intrinsics, distortion, rvec, tvec, false);
+}
